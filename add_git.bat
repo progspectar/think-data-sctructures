a35857be456cd9aa4581bd63bfde5f8f05c6ed46
progspectar <59685434+progspectar@users.noreply.github.com>
2023-10-12 05:39:17 +0700

    solution #3  extends AbstractList<T> - exercise 2: implement a List that uses a Java array

---
+++ add_git.bat
@@ -0,0 +1,7 @@
+echo "# think-data-sctructures" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:progspectar/think-data-sctructures.git
+git push -u origin main
